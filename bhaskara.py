125be39ec5cbeced3e2c62cb19faed35ebf94690
Mariana <mariana.d.penha@gmail.com>
2021-02-16 18:27:06 -0300

    Estou enviando o incício do arquivo

---
+++ bhaskara.py
@@ -0,0 +1,12 @@
+from math import sqrt
+
+b = int(input("Entre com o valor de B"))
+a = int(input("Entre com o valor de A"))
+c = int(input("Entre com o valor de C"))
+
+delta = ((b**2) - 4*a*c
+
+r = (b**2)-4*a*c
+math.sqrt(r)
+
+print(r)
