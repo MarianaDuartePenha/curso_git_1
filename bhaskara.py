8cfae5ab3bc24bcbfb3e2abe69b324f84f9e055d
Mariana <mariana.d.penha@gmail.com>
2021-02-16 19:57:41 -0300

    final

--- bhaskara.py
+++ bhaskara.py
@@ -1,12 +1,18 @@
-from math import sqrt
+import math
 
 b = int(input("Entre com o valor de B"))
 a = int(input("Entre com o valor de A"))
 c = int(input("Entre com o valor de C"))
 
-delta = ((b**2) - 4*a*c
+delta = (b**2) - 4*a*c
+raiz = math.sqrt(delta)
 
-r = (b**2)-4*a*c
-math.sqrt(r)
+x_1 = (- b - raiz)/(2*a)
+x_2 = (- b + raiz)/(2*a)
 
-print(r)
+print(x_1 , x_2)
+
+if x_1 >0:
+    print('Este é o valor correto',x_1)
+if x_2 > 0:
+    print('Utilize este valor',x_2)
